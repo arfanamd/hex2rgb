7e45c4dae8a5fcc09ea3a6d6dccc2aa7ca8e85fd
Arfan Ahmad <arfanachmad11@gmail.com>
2022-06-07 22:28:16 +0700

    Bug fixed! Yeay..
    change:
      hex2rgb.rs
        - Fix bug program didn't read the arguments.
        - Add more verbose help page. hopefully (0,o)
    
    todo:
      - Code cleaning.
      - Add comments.

--- hex2rgb.rs
+++ hex2rgb.rs
@@ -1,5 +1,4 @@
 // https://stackoverflow.com/questions/27043268/convert-a-string-to-int
-use std::env;
 
 fn manhelp() {
     eprintln!("
@@ -8,12 +7,17 @@ fn manhelp() {
     rgb2hex <red> <green> <blue>
 
   DESCRIPTION
-    Show the preview color from the inputed value(s).
+    Color preview from the inputed value(s).
 
   EXAMPLE
-    $ hex2rgb #ffffff #ababab
+    $ hex2rgb \"#ffffff\" abcabc
+    \x1b[48;2;255;255;255m    \x1b[0m    255 255 255    #ffffff
+    \x1b[48;2;171;202;188m    \x1b[0m    171 202 188    #ababab
+
     $ rgb2hex 255 255 255
+    \x1b[48;2;255;255;255m    \x1b[0m    255 255 255    #ffffff
     ");
+    std::process::exit(0);
 }
 
 fn rgb2hex(v1: String, v2: String, v3: String) {
@@ -30,8 +34,8 @@ fn rgb2hex(v1: String, v2: String, v3: String) {
         std::process::exit(1);
     });
 
-    if r < 256 && g < 256 && b < 256 {
-        println!("\x1b[48;2;{};{};{}m    \x1b[0m    {:>3} {:>3} {:>3}    #{:2X}{:2X}{:2X}",
+    if r <= 0xff && g <= 0xff && b <= 0xff {
+        println!("\x1b[48;2;{};{};{}m    \x1b[0m    {:>3} {:>3} {:>3}    #{:0>2X}{:0>2X}{:0>2X}",
                  r, g, b, r, g, b, r, g, b);
     } else {
         println!("[\x1b[31mFatal\x1b[0m]: The value must less than 256!");
@@ -42,23 +46,23 @@ fn rgb2hex(v1: String, v2: String, v3: String) {
 fn hex2rgb(value: String) {
     let convert = | ch | {
         match ch {
-            '0'|'1'|'2'|'3'|'4' => ch.to_digit(10).unwrap(),
-            '5'|'6'|'7'|'8'|'9' => ch.to_digit(10).unwrap(),
-            'A'|'a' => 10,
-            'B'|'b' => 11,
-            'C'|'c' => 12,
-            'D'|'d' => 13,
-            'E'|'e' => 14,
-            'F'|'f' => 15,
-            _ => 0,
+            '0'|'1'|'2'|'3'|'4' => ch.to_digit(16).unwrap(),
+            '5'|'6'|'7'|'8'|'9' => ch.to_digit(16).unwrap(),
+            'A'|'a' => 0xa,
+            'B'|'b' => 0xb,
+            'C'|'c' => 0xc,
+            'D'|'d' => 0xd,
+            'E'|'e' => 0xe,
+            'F'|'f' => 0xf,
+            _ => 0x0,
         }
     };
-    if value.len() == 7 {
+    if value.len() == 0x7 {
         let mut v_iter = value.chars();
         if v_iter.next() == Some('#') {
-            let mut r: u32 = 0;
-            let mut g: u32 = 0;
-            let mut b: u32 = 0;
+            let mut r: u32 = 0x0;
+            let mut g: u32 = 0x0;
+            let mut b: u32 = 0x0;
 
             r += convert(v_iter.next().unwrap()) * 0x10;
             r += convert(v_iter.next().unwrap()) * 0x1;
@@ -72,24 +76,41 @@ fn hex2rgb(value: String) {
         } else {
             println!("[\x1b[31mFatal\x1b[0m]: {} Wrong input!", value);
         }
+    } else if value.len() == 0x6 {
+        let mut v_iter = value.chars();
+        let mut r: u32 = 0x0;
+        let mut g: u32 = 0x0;
+        let mut b: u32 = 0x0;
+
+        r += convert(v_iter.next().unwrap()) * 0x10;
+        r += convert(v_iter.next().unwrap()) * 0x1;
+        g += convert(v_iter.next().unwrap()) * 0x10;
+        g += convert(v_iter.next().unwrap()) * 0x1;
+        b += convert(v_iter.next().unwrap()) * 0x10;
+        b += convert(v_iter.next().unwrap()) * 0x1;
+
+        println!("\x1b[48;2;{};{};{}m    \x1b[0m    {:>3} {:>3} {:>3}    #{}",
+                r, g, b, r, g, b, value);
     } else {
         println!("[\x1b[31mFatal\x1b[0m]: {} Wrong input!", value);
     }
 }
 
 fn main() {
-    let argv: Vec<String> = env::args().collect();
+    let argv: Vec<String> = std::env::args().collect();
     let argc: usize = argv.len();
 
-    if argv[0].contains("hex2rgb") {
-        println!("h2r");
-        for arg in 1..argc {
-            hex2rgb(argv[arg].clone());
-        }
-    } else if argv[0].contains("rgb2hex") {
-        println!("r2h");
-        if argc == 4 {
-            rgb2hex(argv[1].clone(), argv[2].clone(), argv[3].clone());
+    if argc != 0x1 {
+        if argv[0].contains("hex2rgb") {
+            for arg in 1..argc {
+                hex2rgb(argv[arg].clone());
+            }
+        } else if argv[0].contains("rgb2hex") {
+            if argc == 0x4 {
+                rgb2hex(argv[1].clone(), argv[2].clone(), argv[3].clone());
+            } else {
+                manhelp();
+            }
         } else {
             manhelp();
         }

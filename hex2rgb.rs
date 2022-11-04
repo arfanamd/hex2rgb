fe2efd7cdaf597947e9526a7643121ff2a0cbdfa
Arfan Ahmad <arfanachmad11@gmail.com>
2022-11-05 01:24:08 +0700

    code improvement
    * improve function hex2rgb()
    * improve function rgb2hex()
    * new function print_result()
    * small change error message

--- hex2rgb.rs
+++ hex2rgb.rs
@@ -40,101 +40,90 @@ fn exerr(msg: &str) {
 	process::exit(CANCEL_OPERATION);
 }
 
-fn rgb2hex(v1: String, v2: String, v3: String) {
-	// Thanks to this thread:
-	// stackoverflow.com/questions/27043268/convert-a-string-to-int
-	let r = v1.parse::<u32>().unwrap_or_else(|_e| {
-		exerr(&format!("{} Wrong input!", v1));
-	});
-	let g = v2.parse::<u32>().unwrap_or_else(|_e| {
-		exerr(&format!("{} Wrong input!", v2));
-	});
-	let b = v3.parse::<u32>().unwrap_or_else(|_e| {
-		exerr(&format!("{} Wrong input!", v3));
-	});
-
-	if r <= 0xff && g <= 0xff && b <= 0xff {
-		println!("\x1b[48;2;{};{};{}m        \x1b[0m    \
-								 {:>3} {:>3} {:>3}    #{:0>2x}{:0>2x}{:0>2x}",
-								 r, g, b, r, g, b, r, g, b);
-	} else {
-		exerr("The value must be less than 256!");
-	}
+fn print_result(r: &u32, g: &u32, b: &u32) {
+	println!("\x1B[48;2;{};{};{}m        \x1B[0m  \
+					 {:>3} {:>3} {:>3}  #{:0>2x}{:0>2x}{:0>2x}",
+					 r, g, b, r, g, b, r, g, b);
 }
-
-fn hex2rgb(value: String) {
-	// convert each character to a decimal.
-	let convert = | ch | {
-		match ch {
-			'0'|'1'|'2'|'3'|'4' => ch.to_digit(16).unwrap(),
-			'5'|'6'|'7'|'8'|'9' => ch.to_digit(16).unwrap(),
-			'A'|'a' => 0xa,
-			'B'|'b' => 0xb,
-			'C'|'c' => 0xc,
-			'D'|'d' => 0xd,
-			'E'|'e' => 0xe,
-			'F'|'f' => 0xf,
-			_ => 0x0,
+fn rgb2hex(r: String, g: String, b: String) {
+	/* convert string digit into actual digit */
+	let str2digit = | st: &String | {
+		match st.parse::<u32>() {
+			Ok(good) => return good,
+			Err(bad) => {
+				exerr(&format!("{}", bad));
+				return 0;
+			},
 		}
 	};
-	// if the value starts with the pawn sign '#'
-	if value.len() == 0x7 {
-		// string iteration.
-		let mut v_iter = value.chars();
-		if v_iter.next() == Some('#') {
-			let mut r: u32 = 0x0;
-			let mut g: u32 = 0x0;
-			let mut b: u32 = 0x0;
 
-			// 16^0 && 16^1
-			r += convert(v_iter.next().unwrap()) * 0x10;
-			r += convert(v_iter.next().unwrap()) * 0x1;
-			g += convert(v_iter.next().unwrap()) * 0x10;
-			g += convert(v_iter.next().unwrap()) * 0x1;
-			b += convert(v_iter.next().unwrap()) * 0x10;
-			b += convert(v_iter.next().unwrap()) * 0x1;
+	let red   = str2digit(&r);
+	let green = str2digit(&g);
+	let blue  = str2digit(&b);
 
-			println!("\x1b[48;2;{};{};{}m        \x1b[0m    \
-										 {:>3} {:>3} {:>3}    {}",
-										 r, g, b, r, g, b, value);
-		} else {
-			exerr(&format!("{} Wrong input!", value));
-		}
-		// if the value not starts with the pawn sign '#'
-	} else if value.len() == 0x6 {
-		// string iteration.
-		let mut v_iter = value.chars();
-		let mut r: u32 = 0x0;
-		let mut g: u32 = 0x0;
-		let mut b: u32 = 0x0;
-
-		// 16^0 && 16^1
-		r += convert(v_iter.next().unwrap()) * 0x10;
-		r += convert(v_iter.next().unwrap()) * 0x1;
-		g += convert(v_iter.next().unwrap()) * 0x10;
-		g += convert(v_iter.next().unwrap()) * 0x1;
-		b += convert(v_iter.next().unwrap()) * 0x10;
-		b += convert(v_iter.next().unwrap()) * 0x1;
-
-		println!("\x1b[48;2;{};{};{}m        \x1b[0m    \
-								 {:>3} {:>3} {:>3}    #{}",
-								 r, g, b, r, g, b, value);
+	if red <= 0xff && green <= 0xff && blue <= 0xff {
+		print_result(&red, &green, &blue);
 	} else {
-		exerr(&format!("{} Wrong input!", value));
+		exerr("each value must be less than 255!");
 	}
+}
+fn hex2rgb(hex_color: String) {
+	/* convert hex character into hex digit */
+	let char2digit = | ch: char | {
+		match ch.to_digit(16) {
+			Some(d) => return d,
+			None    => {
+				exerr(&format!("invalid hex value of {}", hex_color));
+				return 0;
+			},
+		}
+	};
+
+	let mut r: u32 = 0x0;
+	let mut g: u32 = 0x0;
+	let mut b: u32 = 0x0;
+
+	/* 16^1 && 16^0 */
+	match hex_color.len() {
+		6 => {
+			let mut iter = hex_color.chars();
+			r += char2digit(iter.next().unwrap()) << 0x4;
+			r += char2digit(iter.next().unwrap());
+			g += char2digit(iter.next().unwrap()) << 0x4;
+			g += char2digit(iter.next().unwrap());
+			b += char2digit(iter.next().unwrap()) << 0x4;
+			b += char2digit(iter.next().unwrap());
+		},
+		7 => {
+			let mut iter = hex_color.chars();
+			if iter.next() != Some('#') {
+				exerr(&format!("invalid hex value of {}", hex_color));
+			}
+			r += char2digit(iter.next().unwrap()) << 0x4;
+			r += char2digit(iter.next().unwrap());
+			g += char2digit(iter.next().unwrap()) << 0x4;
+			g += char2digit(iter.next().unwrap());
+			b += char2digit(iter.next().unwrap()) << 0x4;
+			b += char2digit(iter.next().unwrap());
+		},
+		_ => {
+			exerr(&format!("invalid hex value of {}", hex_color));
+		},
+	}
+	print_result(&r, &g, &b);
 }
 
 fn main() {
 	let argv: Vec<String> = env::args().collect();
 	let argc: usize = argv.len();
 
-	if argc != 0x1 {
+	if argc != 1 {
 		if argv[0].contains("hex2rgb") {
 			for arg in 1..argc {
 				hex2rgb(argv[arg].clone());
 			}
 		} else if argv[0].contains("rgb2hex") {
-			if argc == 0x4 {
+			if argc == 4 {
 				rgb2hex(argv[1].clone(), argv[2].clone(), argv[3].clone());
 			} else {
 				print_help();
